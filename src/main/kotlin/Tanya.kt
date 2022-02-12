e2a897655c8dab749873d81a68c77a1b5f6182c9
tanyamayiana <tanyamayiana>
2022-02-12 17:58:39 +0300

    first commit

---
+++ src/main/kotlin/Tanya.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name:String = "Tanyasis Mayiana "
+    println(name)
+    var number:String = "0718777705"
+    println(number)
+    var age = 21
+    println(age)
+    var dead:Boolean = false
+    println(dead)
+    var temperature:Byte = 25
+    println(temperature)
+
+}
+
+
